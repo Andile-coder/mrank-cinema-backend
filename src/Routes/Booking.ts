11f5bf7915985825cc6669c3d8d961943dcb8467
AndileMasela <maselaandile@gmail.com>
2022-09-11 02:45:10 +0200

    Update Booking.ts

--- src/Routes/Booking.ts
+++ src/Routes/Booking.ts
@@ -4,12 +4,11 @@ import { BookingController } from '../Controllers/Booking';
 const router = Router();
 const Controller = new BookingController();
 
-router.get('/api/movies', (request, response, next) => {
-  Controller.GetMovies(request, response, next);
-});
-
 router.post('/api/movie', json(), (request, response, next) => {
   Controller.Book(request, response, next);
+});
+router.get('/api/movies', (request, response, next) => {
+  Controller.GetMovies(request, response, next);
 });
 
 router.get('/api/seats', json(), (request, response, next) => {
